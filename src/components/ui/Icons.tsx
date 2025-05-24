c9a374ff3cac4e5ba9e8ba8d996e5f7782bdfa73
MostafaHussien2000 <mabozeed70@gmail.com>
2025-05-24 22:00:49 +0300

    Adding App Icons

---
+++ src/components/ui/Icons.tsx
@@ -0,0 +1,21 @@
+import {
+  TbCalendar,
+  TbClock,
+  TbCommand,
+  TbSearch,
+  TbStar,
+  TbStarFilled,
+  TbStarHalfFilled,
+} from "react-icons/tb";
+
+const Icons = {
+  Search: TbSearch,
+  Command: TbCommand,
+  Star: TbStar,
+  HalfStar: TbStarHalfFilled,
+  StarFilled: TbStarFilled,
+  Calendar: TbCalendar,
+  Time: TbClock,
+};
+
+export default Icons;
