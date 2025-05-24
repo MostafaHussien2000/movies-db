e8a56476f50cf1473f50e472910ea62aabb664af
MostafaHussien2000 <mabozeed70@gmail.com>
2025-05-24 19:22:18 +0300

    Revert "Merge pull request #1 from MostafaHussien2000/feature/tmdb-api-interface"
    This is reverts commit 4203f4914217b49ae93fc26d15e1ef6ec69813a4, reversing
    changes made to c7cd2535900080302e2da6bb36be2858a7560eb5.

--- src/services/activityHistory/config.ts
+++
@@ -1,3 +0,0 @@
-export const RECENT_SEARCHES_KEY = "recent_searches";
-export const RECENT_VIEWS_KEY = "recent_views";
-export const MAX_ITEMS = 10;
--- src/services/tmdb/config.ts
+++
@@ -1,3 +0,0 @@
-export const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-export const BASE_URL = "https://api.themoviedb.org/3";
-export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
